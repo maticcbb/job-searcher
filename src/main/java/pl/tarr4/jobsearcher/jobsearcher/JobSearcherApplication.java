610664ab47d6c18146a56423af9a42af25539c41
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-23 19:04:37 +0200

    Initial commit

---
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobSearcherApplication.java
@@ -0,0 +1,13 @@
+package pl.tarr4.jobsearcher.jobsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tarr4/jobsearcher/jobsearcher/JobSearcherApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tarr4.jobsearcher.jobsearcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobSearcherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
