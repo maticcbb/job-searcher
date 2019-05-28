0e413eca4d53f654373c5bafe7dfd183a083b8f7
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-29 00:40:35 +0200

    Add methode to find all jobs by location from database

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobRepository.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JobRepository extends CrudRepository<Job, Long> {
+
+    Iterable<Job> findAllByLocation(String location);
+
+
 
 }
