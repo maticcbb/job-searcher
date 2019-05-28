c98841186f14b87a65d60140f3151b7983160abc
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-28 16:50:55 +0200

    Create DTO Search class for primary searching

---
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/PrimarySearchDTO.java
@@ -0,0 +1,15 @@
+package pl.tarr4.jobsearcher.jobsearcher;
+
+import lombok.Data;
+
+@Data
+public class PrimarySearchDTO {
+
+    private String keywords;
+    private String location;
+
+    public PrimarySearchDTO(String keywords, String location) {
+        this.keywords = keywords;
+        this.location = location;
+    }
+}
