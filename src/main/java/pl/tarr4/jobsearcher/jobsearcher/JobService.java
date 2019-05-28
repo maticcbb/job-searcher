f32ec720910939fe1bd28497d17db1ba366a6980
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-28 16:51:57 +0200

    Add JobFilter object to findJobs() methode

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobService.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobService.java
@@ -1,32 +1,38 @@
 package pl.tarr4.jobsearcher.jobsearcher;
 
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class JobService {
 
+    @Autowired
     JobRepository jobRepository;
 
     public JobService(JobRepository jobRepository) {
         this.jobRepository = jobRepository;
     }
 
-  /*  public Iterable<Job> findJobs(JobFilter jobFilter){
+    public Iterable<Job> findAll(){
+       return jobRepository.findAll();
+    }
+
+
+    public void findJobs(JobFilter jobFilter) throws JsonProcessingException {
         // TODO: if local database has at least 1 result, then use local database instead first
         // TODO: make request to the API using jobFilter criteria
         // TODO: save results in the database
 
         RestTemplate restTemplate = new RestTemplate();
-        String jsonToSent = "{" +
-                "\"keywords\": \"developer \",\n" +
-                "\"location\": \"Bydgoszcz\",\n" +
-                "\"radius\": \"80\",\n" +
-                "\"salary\": \"2000\",\n" +
-                "\"page\": \"1\"" +
-                "}";
+        String jsonToSent = jobFilter.mapObjectToJson("dev","kielce");
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
@@ -41,6 +47,10 @@ public class JobService {
 
         JobContainer job = response.getBody();
 
-    }*/
+        jobRepository.saveAll(job.getJobs());
+
+    }
+
+
 
 }
