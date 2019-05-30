711873a57c6e8a8628de535f0fc265f982c995de
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-30 16:50:09 +0200

    JobController code refactoring

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobController.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobController.java
@@ -3,10 +3,13 @@ package pl.tarr4.jobsearcher.jobsearcher;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 
 @RestController
-@RequestMapping("/jobs")
+@RequestMapping("/api/jobs")
 public class JobController {
+
 
     private JobService jobService;
 
@@ -14,21 +17,21 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping
+/*    @GetMapping
     public Iterable<Job> getAllJobs() {
         return jobService.findAll();
-    }
-
+    }*/
 
     @PostMapping
-    public String fetchAllJobs(@RequestParam String keywords , @RequestParam String location) throws JsonProcessingException {
+    public String fetchAllJobs(@RequestParam String keywords, @RequestParam String location) throws JsonProcessingException {
         jobService.fetchAllJobs(new PrimarySearchDTO(keywords, location));
         return "Saved";
     }
 
-    @GetMapping("/location/{location}")
-    public Iterable<Job> getAllByLocation(@PathVariable String location) {
-        return jobService.findAllByLocation(location);
+
+    @GetMapping
+    public Collection<Job> getJobsByKeywordAndLocation(@RequestParam String keyword, @RequestParam String location) throws JsonProcessingException {
+        return jobService.getJobsByKeywordAndLocation(keyword, location);
     }
 
 }
