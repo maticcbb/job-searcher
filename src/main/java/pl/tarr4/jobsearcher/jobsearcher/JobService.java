530609127e05c9727233fe2a0fbe5dc84cf90a04
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-30 16:50:41 +0200

    Job Service methodes for fetching all pages from jooble  Api

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobService.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobService.java
@@ -1,12 +1,10 @@
 package pl.tarr4.jobsearcher.jobsearcher;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,46 +12,76 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class JobService {
 
-    @Autowired
+
     JobRepository jobRepository;
 
     public JobService(JobRepository jobRepository) {
         this.jobRepository = jobRepository;
     }
 
-    public Iterable<Job> findAll(){
-       return jobRepository.findAll();
+    public Iterable<Job> findAll() {
+        return jobRepository.findAll();
     }
 
-    public Iterable<Job> findAllByLocation(String location){
-        return jobRepository.findAllByLocation(location);
+
+    public Collection<Job> findAllByKeywordAndLocation(String keyword, String location) {
+        return jobRepository.findJobsByKeywordAndLocation(keyword, location);
     }
 
 
     public void fetchAllJobs(PrimarySearchDTO jobFilter) throws JsonProcessingException {
-        // TODO: if local database has at least 1 result, then use local database instead first
-        // TODO: make request to the API using jobFilter criteria
-        // TODO: save results in the database
+        JobContainer jobContainer = getJobsFromApi(jobFilter);
+        saveJobsFromAllPages(jobFilter, jobContainer);
+    }
+
+    public Collection<Job> getJobsByKeywordAndLocation(String keyword, String location) throws JsonProcessingException {
+        Collection<Job> databaseResult = jobRepository.findJobsByKeywordAndLocation(keyword, location);
+
+        if (databaseResult.isEmpty() /*|| isDataAfterDate(databaseResult)*/) {
+            fetchAllJobs(new PrimarySearchDTO(keyword, location));
+            return findAllByKeywordAndLocation(keyword, location);
+        }
+        return findAllByKeywordAndLocation(keyword, location);
+    }
+
+
+    public boolean isDataAfterDate(Collection<Job> fetchedJob) {
+        return LocalDateTime.now().isAfter(fetchedJob.iterator().next().getFetchDate().plusMinutes(1));
+    }
+
+
+    public void saveJobsFromAllPages(PrimarySearchDTO jobFilter, JobContainer jobContainer) {
+        int allJobs = jobContainer.getTotalCount();
+        Integer page = 2;
+        if (allJobs > 20) {
+            for (int i = 0; i < allJobs; i += 20) {
+                jobFilter.setPage(page.toString());
+                jobRepository.saveAll(getJobsFromApi(jobFilter).getJobs());
+                page++;
+            }
+        }
+        jobRepository.saveAll(getJobsFromApi(jobFilter).getJobs());
+    }
+
+    public void updateJobs(PrimarySearchDTO jobFilter){
+       JobContainer jobsFromApi = getJobsFromApi(jobFilter);
+
+    }
+
+    public JobContainer getJobsFromApi(PrimarySearchDTO jobFilter) {
 
         RestTemplate restTemplate = new RestTemplate();
-//        String jsonToSent = jobFilter.mapObjectToJson("developer","bydgoszcz");
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
-
-//        HttpEntity httpEntity = new HttpEntity(jsonToSent, httpHeaders);
 
         ResponseEntity<JobContainer> response = restTemplate.postForEntity(
                 "https://pl.jooble.org/api/ea1d5e70-b947-4a4d-9a60-eb8304182043",
                 jobFilter,
                 JobContainer.class);
 
-        JobContainer job = response.getBody();
-
-        jobRepository.saveAll(job.getJobs());
-
+        return response.getBody();
     }
-
 
 
 }
