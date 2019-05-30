00cfd5c6bf562c7e2adf9587d2fec557dbc3fabb
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-30 16:47:26 +0200

    Add query for searching keywords in title

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobRepository.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobRepository.java
@@ -1,15 +1,20 @@
 package pl.tarr4.jobsearcher.jobsearcher;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.Collection;
+
 
 public interface JobRepository extends CrudRepository<Job, Long> {
 
     Iterable<Job> findAllByLocation(String location);
+
+    @Query("SELECT j FROM Job j WHERE lower(j.title) LIKE lower(concat('%',:keyword,'%')) AND j.location = :location ")
+    //OR lower(j.company) LIKE lower(concat('%',:keyword,'%')) AND  j.location = :location
+    Collection<Job> findJobsByKeywordAndLocation(@Param("keyword") String keyword , @Param("location") String location);
 
 
 
