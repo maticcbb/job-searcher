54dfa5b658daba9743f7a917c2e85dd262f72dff
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-30 17:45:31 +0200

    Properties change and Job  char length

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/Application.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/Application.java
@@ -11,27 +11,10 @@ import org.springframework.web.client.RestTemplate;
 
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String args[]) {
-        RestTemplate restTemplate = new RestTemplate();
-        String jsonToSent = "{" +
-                "\"keywords\": \"IT \",\n" +
-                "\"location\": \"Bydgoszcz\",\n" +
-                "\"radius\": \"80\",\n" +
-                "\"salary\": \"100\",\n" +
-                "\"page\": \"1\"" +
-                "}";
+    public static void main(String[] args) {
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(HttpHeaders.CONTENT_TYPE , "application/json");
-
-        HttpEntity httpEntity = new HttpEntity(jsonToSent, httpHeaders);
-
-        ResponseEntity exchange = restTemplate.exchange(
-                "https://pl.jooble.org/api/ea1d5e70-b947-4a4d-9a60-eb8304182043",
-                HttpMethod.POST,
-                httpEntity,
-                Job.class);
     }
+
+
 }
