e5c9d654514203569f42ad84ee9e8b42080e8787
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-06-01 00:08:24 +0200

    Create class SearchFormControiller for views with searching

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/SearchFormController.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/SearchFormController.java
@@ -1,14 +1,31 @@
 package pl.tarr4.jobsearcher.jobsearcher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/job")
 public class SearchFormController {
 
-    @GetMapping("/app/search")
+    JobService jobService;
+
+    public SearchFormController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping()
     public String search(){
         return "search";
+    }
+
+    @GetMapping("/result")
+    public String result(Model model , @RequestParam String keyword,@RequestParam String location) throws JsonProcessingException {
+        model.addAttribute("allJobs", jobService.getJobsByKeywordAndLocation(keyword, location));
+        return "result";
     }
 
 }
