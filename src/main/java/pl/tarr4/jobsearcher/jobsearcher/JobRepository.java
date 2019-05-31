4662ce9b8f01f5ff18d5097d51b667ebbcf6d084
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-06-01 00:06:18 +0200

    Add keyword searching in snippets in query to database

--- src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobRepository.java
+++ src/main/java/pl/tarr4/jobsearcher/jobsearcher/JobRepository.java
@@ -12,8 +12,9 @@ public interface JobRepository extends CrudRepository<Job, Long> {
 
     Iterable<Job> findAllByLocation(String location);
 
-    @Query("SELECT j FROM Job j WHERE lower(j.title) LIKE lower(concat('%',:keyword,'%')) AND j.location = :location ")
+    @Query("SELECT j FROM Job j WHERE lower(j.title) LIKE lower(concat('%',:keyword,'%')) AND j.location = :location OR lower(j.snippet) LIKE lower(concat('%',:keyword,'%')) AND  j.location = :location ")
     //OR lower(j.company) LIKE lower(concat('%',:keyword,'%')) AND  j.location = :location
+
     Collection<Job> findJobsByKeywordAndLocation(@Param("keyword") String keyword , @Param("location") String location);
 
 
